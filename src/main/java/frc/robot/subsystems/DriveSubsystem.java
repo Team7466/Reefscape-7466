0358996b9045a57f4136b36182f25732be1ca5ae
Yigit <yigit.olc@hotmail.com>
2025-01-17 22:15:47 +0000

    ..

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,24 +7,63 @@ package frc.robot.subsystems;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPLTVController;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.kinematics.Kinematics;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
+
   private final DifferentialDriveOdometry odometry;
   private final AHRS gyro;
+  private final SparkMax leftMotor;
+  private final SparkMax leftMotor2;
+  private final SparkMax rightMotor;
+  private final SparkMax rightMotor2;
+  private final RelativeEncoder leftEncoder;
+  private final RelativeEncoder rightEncoder;
+  private final DifferentialDriveKinematics kinematics;
+  private RobotConfig config;
 
   public DriveSubsystem() {
-    // All other subsystem initialization
-    // ...
 
-    // Load the RobotConfig from the GUI settings. You should probably
-    // store this in your Constants file
-    RobotConfig config;
-    try {
+    kinematics = new DifferentialDriveKinematics(Constants.DriveConstants.trackWidthMeters);
+
+    // navX MXP using SPI
+    gyro = new AHRS(NavXComType.kMXP_SPI);
+
+    // All other subsystem initialization
+    leftMotor= new SparkMax(3, MotorType.kBrushless);
+    leftMotor2= new SparkMax(4, MotorType.kBrushless);
+    rightMotor= new SparkMax(5, MotorType.kBrushless);
+    rightMotor2= new SparkMax(6, MotorType.kBrushless);
+
+    leftEncoder=leftMotor.getEncoder();
+    rightEncoder=rightMotor.getEncoder();
+
+    odometry = new DifferentialDriveOdometry(gyro.getRotation2d(),
+    leftEncoder.getPosition(), rightEncoder.getPosition()
+  );
+
+
+
+
+    try{
       config = RobotConfig.fromGUISettings();
     } catch (Exception e) {
       // Handle exception as needed
@@ -33,37 +72,35 @@ public class DriveSubsystem extends SubsystemBase {
 
     // Configure AutoBuilder last
     AutoBuilder.configure(
-        this::getPose, // Robot pose supplier
-        this::resetPose, // Method to reset odometry (will be called if your auto has a starting
-        // pose)
-        this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-        (speeds, feedforwards) ->
-            driveRobotRelative(
-                speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds.
-        // Also optionally outputs individual module feedforwards
-        new PPLTVController(
-            0.02), // PPLTVController is the built in path following controller for differential
-        // drive trains
-        config, // The robot configuration
-        () -> {
-          // Boolean supplier that controls when the path will be mirrored for the red alliance
-          // This will flip the path being followed to the red side of the field.
-          // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+            this::getPose, // Robot pose supplier
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getCurrentSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+            new PPLTVController(0.02), // PPLTVController is the built in path following controller for differential drive trains
+            config, // The robot configuration
+            () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-          var alliance = DriverStation.getAlliance();
-          if (alliance.isPresent()) {
-            return alliance.get() == DriverStation.Alliance.Red;
-          }
-          return false;
-        },
-        this // Reference to this subsystem to set requirements
-        );
-  }
+              var alliance = DriverStation.getAlliance();
+              if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+              }
+              return false;
+            },
+            this // Reference to this subsystem to set requirements
+    );
+}
+
+
+   
 
   private void driveRobotRelative(ChassisSpeeds speeds) {
 
     throw new UnsupportedOperationException("Unimplemented method 'driveRobotRelative'");
   }
+ 
 
   public Pose2d getPose() {
     return odometry.getPoseMeters();
@@ -71,9 +108,19 @@ public class DriveSubsystem extends SubsystemBase {
 
   public void resetPose(Pose2d pose) {
     System.out.println(pose);
-    odometry.resetPosition(gyro.getRotation2d(), getPositions(), pose);
+    odometry.resetPosition(gyro.getRotation2d(), getCurrentPositions(), pose);
+  }
+   
+  public ChassisSpeeds getCurrentSpeeds(){
+    DifferentialDriveWheelSpeeds speeds = new DifferentialDriveWheelSpeeds(leftMotor.getEncoder().getVelocity(), 0);
+    return kinematics.toChassisSpeeds( speeds);
   }
 
+  public DifferentialDriveWheelPositions getCurrentPositions(){
+   DifferentialDriveWheelPositions positions = new DifferentialDriveWheelPositions(leftEncoder.getPosition(), rightEncoder.getPosition());  
+   return positions ;
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
