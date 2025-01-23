d71ab923b30af90bf908983c52ed3f685f42fe86
Yigit <yigit.olc@hotmail.com>
2025-01-23 00:08:43 +0000

    fix gear ratio,max speed and add conversion factors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -20,7 +22,10 @@ public final class Constants {
 
   public static class DriveConstants {
     public static final double trackWidthMeters = 0.5308; // meters
-    public static final double gearRatio = 8.75;
-    public static final double maxSpeed = 4.36; // meters per second
+    public static final double gearRatio = 10.71;
+    public static final double maxSpeed = 4.20; // meters per second
+    public static final double wheelCircumference = (6 * Math.PI);
+    public static final double velocityConversionFactor = Units.inchesToMeters((1.0/gearRatio)*(wheelCircumference)/ 60.0); 
+    public static final double positionConversionFactor = Units.inchesToMeters(1.0/gearRatio)*(wheelCircumference);  
   }
 }
